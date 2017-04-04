bb19df469a1c923085200662775889dfbb729905
Stefan Wendler <sw@kaltpost.de>
2017-04-04 22:14:16 +0200

    Player now curses based.

--- moppy-python/src/moppy/player.py
+++ moppy-python/src/moppy/player.py
@@ -1,9 +1,8 @@
 import argparse
 import operator
-import colorama
+import curses
 import mido
-
-colorama.init()
+import os
 
 
 class MoppySysfsPort(mido.ports.BaseOutput):
@@ -34,204 +33,298 @@ class MoppySysfsPort(mido.ports.BaseOutput):
         self._write_sysfs("reset", "ctrl")
 
 
-def analyze(midi):
+class Player:
 
-    stats = {
-        "channels": {},
-        "octaves": {},
-    }
+    def __init__(self, port, filename, ch_max=4, ch_filter=None, update_hook=None):
 
-    for msg in midi:
+        self.port = port
+        self.filename = filename
 
-        if not msg.is_meta and msg.type == 'note_on':
+        if ch_filter is None:
+            self.ch_filter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15]
+        else:
+            self.ch_filter = ch_filter
 
-            if msg.channel in stats["channels"]:
-                stats["channels"][msg.channel] += 1
-            else:
-                stats["channels"][msg.channel] = 1
+        self.ch_max = ch_max
+        self.update_hook = update_hook
+        self.playing = False
 
-            octave = int(msg.note / 12) - 1
+    def play(self, midi=None, info=None):
 
-            if octave in stats["octaves"]:
-                stats["octaves"][octave] += 1
-            else:
-                stats["octaves"][octave] = 1
+        if midi is None:
+            midi = mido.MidiFile(self.filename)
 
-            # print(msg)
+        if info is None:
+            info = self.analyze(midi)
 
-    print("")
-    print("Results for: %s" % midi.filename)
-    print("             length: %d:%d" % (midi.length // 60, midi.length - (midi.length // 60) * 60))
-    print("          midi type: %d" % midi.type)
-    print("   number of tracks: %d" % len(midi.tracks))
-    print(" number of channels: %d" % (len(stats["channels"])))
-    print("      channels used: %s" % ([x for x in stats["channels"].keys()]))
-    print("  notes in channels: %s" % stats["channels"])
-    print("  number of octaves: %d" % (len(stats["octaves"])))
-    print("       octaves used: %s" % ([x for x in stats["octaves"].keys()]))
+        most_used_channels = sorted(info["channels"].items(), key=operator.itemgetter(1), reverse=True)
 
-    return stats
+        ch_map = {}
 
+        mappend_ch = 0
 
-def constraint_octave(msg):
+        for ch, _ in most_used_channels:
+            if ch in self.ch_filter:
 
-    octave = msg.note // 12 - 1
+                ch_map[ch] = mappend_ch
+                mappend_ch += 1
 
-    if octave < 2:
-        msg.note += (2 - octave) * 12
-    elif octave > 5:
-        msg.note -= (octave - 5) * 12
+                if mappend_ch == self.ch_max:
+                    break
 
-    return msg
+        self.port.reset()
 
-
-def play(port, filename, max_ch, ch_filter=None):
-
-    print("")
-    print("** non-optimized player ")
-    print("")
-
-    if ch_filter is None:
-        ch_filter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-    midi = mido.MidiFile(filename)
-
-    analyze(midi)
-
-    stats = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-
-    print("")
-    print(("c/o  | " + "%02d " * 16) % tuple(range(16)))
-    print("-" * 55)
-
-    port.reset()
-
-    try:
+        self.playing = True
 
         for msg in midi.play():
 
-            if msg.type in ['note_on', 'note_off'] and msg.channel in ch_filter and msg.channel < max_ch:
-
-                octave = msg.note // 12 - 1
-
-                # print(msg.channel, octave)
-                if msg.type == 'note_off':
-                    stats[octave][msg.channel] = 0xff
-                else:
-                    stats[octave][msg.channel] = msg.note
-
-                for o, chan_stat in enumerate(stats):
-
-                    disp = "%02d   | %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X" % \
-                           (tuple([o]) +  tuple(chan_stat))
-
-                    print(disp.replace(" 00", " __").replace("FF", "--"))
-
-                print(colorama.Cursor.UP(len(stats) + 1) + colorama.Cursor.BACK())
-
-                port.send(msg)
-
-    except KeyboardInterrupt:
-        pass
-
-    port.reset()
-
-    print(colorama.Cursor.DOWN(len(stats) + 1))
-
-
-def play_optimized(port, filename, max_ch, ch_filter=None):
-
-    print("")
-    print("** optimized player ")
-    print("")
-
-    if ch_filter is None:
-        ch_filter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15]
-
-    midi = mido.MidiFile(filename)
-
-    info = analyze(midi)
-
-    most_used_channels = sorted(info["channels"].items(), key=operator.itemgetter(1), reverse=True)
-
-    ch_map = {}
-
-    mappend_ch = 0
-
-    for ch, _ in most_used_channels:
-        if ch in ch_filter:
-
-            ch_map[ch] = mappend_ch
-            mappend_ch += 1
-
-            if mappend_ch == max_ch:
+            if not self.playing:
                 break
-
-    stats = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-
-    print("")
-    print(("c/o  | " + "%02d " * 16) % tuple(range(16)))
-    print("-" * 79)
-
-    port.reset()
-
-    try:
-
-        for msg in midi.play():
 
             if msg.type in ['note_on', 'note_off'] and msg.channel in ch_map:
 
-                msg = constraint_octave(msg)
+                msg = self.constraint_octave(msg)
                 msg.channel = ch_map[msg.channel]
 
                 octave = msg.note // 12 - 1
 
-                # print(msg.channel, octave)
-                if msg.type == 'note_off':
-                    stats[octave][msg.channel] = 0xff
+                if self.update_hook is not None:
+                    if msg.type == 'note_on':
+                        self.update_hook(msg.channel, octave, msg.note % 12)
+                    else:
+                        self.update_hook(msg.channel, octave, 12)
+
+                self.port.send(msg)
+
+        self.port.reset()
+
+    def analyze(self, midi):
+
+        stats = {
+            "channels": {},
+            "octaves": {},
+        }
+
+        '''
+        print("")
+        print("Results for: %s" % midi.filename)
+        print("             length: %d:%d" % (midi.length // 60, midi.length - (midi.length // 60) * 60))
+        print("          midi type: %d" % midi.type)
+        print("   number of tracks: %d" % len(midi.tracks))
+        print(" number of channels: %d" % (len(stats["channels"])))
+        print("      channels used: %s" % ([x for x in stats["channels"].keys()]))
+        print("  notes in channels: %s" % stats["channels"])
+        print("  number of octaves: %d" % (len(stats["octaves"])))
+        print("       octaves used: %s" % ([x for x in stats["octaves"].keys()]))
+        '''
+
+        for msg in midi:
+
+            if not msg.is_meta and msg.type == 'note_on':
+
+                if msg.channel in stats["channels"]:
+                    stats["channels"][msg.channel] += 1
                 else:
-                    stats[octave][msg.channel] = msg.note
+                    stats["channels"][msg.channel] = 1
 
-                for o, chan_stat in enumerate(stats):
+                octave = int(msg.note / 12) - 1
 
-                    disp = "%02d   | %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d %03d" % \
-                           (tuple([o]) + tuple(chan_stat))
+                if octave in stats["octaves"]:
+                    stats["octaves"][octave] += 1
+                else:
+                    stats["octaves"][octave] = 1
 
-                    print(disp.replace(" 000", " ___").replace("255", colorama.Fore.GREEN + ".|." + colorama.Fore.RESET))
+                    # print(msg)
 
-                print(colorama.Cursor.UP(len(stats) + 1) + colorama.Cursor.BACK())
+        return stats
 
-                port.send(msg)
+    def constraint_octave(self, msg):
 
-    except KeyboardInterrupt:
-        pass
+        octave = msg.note // 12 - 1
 
-    port.reset()
+        if octave < 2:
+            msg.note += (2 - octave) * 12
+        elif octave > 5:
+            msg.note -= (octave - 5) * 12
 
-    print(colorama.Cursor.DOWN(len(stats) + 1))
+        return msg
 
+    def stop(self):
+        self.playing = False
+
+
+class VisualPlayer(Player):
+
+    def __init__(self, port, filename, ch_max=4, ch_filter=None):
+
+        Player.__init__(self, port, filename, ch_max, ch_filter, self.set_note)
+
+        self.ch_last_oct = {}
+        self.info_height = 15
+
+        self.file_info = [
+            "",
+            (0, 0),
+            0,
+            0,
+            0,
+            "",
+            0,
+            "",
+            ""
+        ]
+
+        self.stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(1)
+        self.stdscr.nodelay(True)
+
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_GREEN)
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLUE)
+        curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_RED)
+
+        self.stdscr.bkgd(curses.color_pair(1))
+        self.show()
+
+    def __del__(self):
+
+        self.hide()
+
+    def hide(self):
+
+        curses.nocbreak()
+        self.stdscr.keypad(0)
+        curses.echo()
+        curses.endwin()
+
+    def show(self):
+
+        self.stdscr.refresh()
+
+        y, x = self.stdscr.getmaxyx()
+
+        self.max_x = x
+        self.max_y = y
+
+        if self.max_x >= 95 and self.max_y >= 35:
+
+            self.win_info = curses.newwin(self.info_height, self.max_x, 1, 0)
+            self.win_info.bkgd(curses.color_pair(1))
+            self.win_info.box()
+            self.win_info.refresh()
+
+            self.win_notes = curses.newwin(self.max_y - (self.info_height + 1), self.max_x, (self.info_height + 1), 0)
+            self.win_notes.bkgd(curses.color_pair(1))
+            self.win_notes.box()
+            self.win_notes.refresh()
+
+            self.win_menu = curses.newwin(1, self.max_x, 0, 0)
+            self.win_menu.bkgd(curses.color_pair(2))
+            self.win_menu.addstr(0, 1, "F10: exit")
+            self.win_menu.refresh()
+
+            self.show_notes()
+            self.update_file_info()
+
+        else:
+            self.hide()
+            print("*** window too small ***")
+            exit(1)
+
+        self.stdscr.refresh()
+
+    def show_notes(self):
+
+        self.ch_last_oct = {}
+
+        y = (self.max_y - self.info_height - 12) // 2
+        x = (self.max_x - 88) // 2
+
+        self.win_notes.addstr(y, x, "c/o    0    1    2    3    4    5    6    7    8    9   10   11" +
+                              "   12   13   14   15  c/o", curses.color_pair(2))
+
+        for o in range(10):
+            self.win_notes.addstr(y + 1 + o, x, " %d " % o, curses.color_pair(2))
+
+            for c in range(16):
+                self.win_notes.addstr(y + 1 + o, x + 5 + c * 5, "   ", curses.color_pair(3))
+
+            self.win_notes.addstr(y + 1 + o, x + 10 + c * 5, " %d " % o, curses.color_pair(2))
+
+        self.win_notes.addstr(y + 11, x, "c/o    0    1    2    3    4    5    6    7    8    9   10   11" +
+                              "   12   13   14   15  c/o", curses.color_pair(2))
+
+        self.win_notes.refresh()
+
+    def set_note(self, ch, oct, note):
+
+        notes = [
+            " C ", " C#", " D ", " D#", " E ", " F ",
+            " F#", " G ", " G#", " A ", " A#", " B ",
+            " * "
+        ]
+
+        y = (self.max_y - self.info_height - 12) // 2 + 1
+        x = (self.max_x - 88) // 2 + 5
+
+        if ch in self.ch_last_oct:
+            self.win_notes.addstr(y + self.ch_last_oct[ch], x + 5 * ch, '   ', curses.color_pair(4))
+
+        self.win_notes.addstr(y + oct, x + 5 * ch, notes[note], curses.color_pair(3))
+        self.ch_last_oct[ch] = oct
+
+        self.win_notes.refresh()
+
+        self.handl_keys()
+
+    def update_file_info(self):
+
+        self.win_info.addstr( 2, 3, "File           : %s" % self.file_info[0])
+        self.win_info.addstr( 3, 3, "Length         : %d:%d" % self.file_info[1])
+        self.win_info.addstr( 4, 3, "Type           : %d" % self.file_info[2])
+        self.win_info.addstr( 5, 3, "Tracks         : %d" % self.file_info[3])
+        self.win_info.addstr( 6, 3, "Channels       : %d" % self.file_info[4])
+        self.win_info.addstr( 7, 3, "Channels used  : %s" % self.file_info[5])
+        self.win_info.addstr( 8, 3, "Octaves        : %d" % self.file_info[6])
+        self.win_info.addstr( 9, 3, "Octaves used   : %s" % self.file_info[7])
+        self.win_info.addstr(10, 3, "Notes in chan. : %s" % self.file_info[8])
+        self.win_info.refresh()
+
+    def handl_keys(self):
+
+        c = self.stdscr.getch()
+
+        if c == curses.KEY_RESIZE:
+            self.show()
+        elif c == curses.KEY_F10:
+            self.stop()
+
+    def play(self, midi=None, info=None):
+
+        if midi is None:
+            midi = mido.MidiFile(self.filename)
+
+        if info is None:
+            info = self.analyze(midi)
+
+        self.file_info = [
+            os.path.basename(self.filename),
+            (midi.length // 60, midi.length - (midi.length // 60) * 60),
+            midi.type,
+            len(midi.tracks),
+            len(info["channels"]),
+            str([x for x in info["channels"].keys()])[1:-1],
+            len(info["octaves"]),
+            str([x for x in info["octaves"].keys()])[1:-1],
+            str(info["channels"])[1:-1]
+        ]
+
+        self.update_file_info()
+
+        Player.play(self, midi, info)
 
 def main():
 
@@ -257,12 +350,15 @@ def main():
     else:
         port = MoppySysfsPort()
 
+    '''
     if args.optimized:
         do_play = play_optimized
     else:
         do_play = play
+    '''
 
-    do_play(port, args.file, args.maxch)
+    vp = VisualPlayer(port, args.file, args.maxch)
+    vp.play()
 
 
 if __name__ == '__main__':
